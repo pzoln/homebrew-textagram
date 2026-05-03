d4dda39b7d69e19e5f06a47747b0373a2165c9c8
pavel <pavel@textagram.dev>
2026-05-02 18:34:45 -0700

    0.1.0-beta.9

--- Formula/tg.rb
+++ Formula/tg.rb
@@ -2,19 +2,19 @@ class Tg < Formula
   desc "Text-based diagram editor for the terminal"
   homepage "https://textagram.md/"
   license "Apache-2.0"
-  version "0.1.0-beta.5"
+  version "0.1.0-beta.9"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/pzoln/tg/releases/download/tg-v0.1.0-beta.5/tg-v0.1.0-beta.5-aarch64-apple-darwin.tar.gz"
-      sha256 "601bc65968c13c34754a602b7c186249540ed22889d29aef0fdd0e1bae92b6bb"
+      url "https://github.com/pzoln/tg/releases/download/tg-v0.1.0-beta.9/tg-v0.1.0-beta.9-aarch64-apple-darwin.tar.gz"
+      sha256 "ef1aacca6f3c108aa9050ac5ea7b54bc885eb935822f7792a63190edec268228"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/pzoln/tg/releases/download/tg-v0.1.0-beta.5/tg-v0.1.0-beta.5-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "59410b9acef042b61b3a7f9d2bb3f983966736e11c5598f9ac5a2b4069ae8b3d"
+      url "https://github.com/pzoln/tg/releases/download/tg-v0.1.0-beta.9/tg-v0.1.0-beta.9-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "ffdc4c386659b6f9da232dd37aca482d8a1ced043c1ed6f2e04a33b61098866a"
     end
   end
 
